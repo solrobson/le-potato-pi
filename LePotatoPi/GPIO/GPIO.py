1a03316a852d6bb506768e488359c9d5fa4556db
solrobson <solrobson@live.com>
2022-12-28 22:20:12 +0000

    adding input support

--- LePotatoPi/GPIO/GPIO.py
+++ LePotatoPi/GPIO/GPIO.py
@@ -4,6 +4,7 @@ from LePotatoPi.GPIO import PulseWidthManagement as Pulse
 
 class GPIO:
   used_pins = {}
+  
   IN = 1
   OUT = 0
 
@@ -12,6 +13,10 @@ class GPIO:
   
   HIGH = 1
   LOW = 0
+
+  PUD_OFF = 0
+  PUD_DOWN = 1
+  PUD_UP = 2
 
   def __init__(self):
     self.mode = self.BOARD
@@ -24,38 +29,55 @@ class GPIO:
     self.mode = mode
     
   
-  def setup(self, pin, mode, initial = None):
-    #setup(pin, GPIO.OUT, initial = GPIO.HIGH)
+  def setup(self, pin, direction, pull_up_down=PUD_OFF, initial=None):
     mapped_pin = consts.LE_POTATO_PIN_TO_RPI_PIN[pin]
-    #print(mapped_pin.chip)
+
+    print('checking for pin')
+    if pin in self.used_pins:
+      print('getting pin')
+      p = self.used_pins[pin]
+      print('releasing pin')
+      p.release()
+      print('pin released')
+      print('deleting pin')
+      del self.used_pins[pin]
+      print('pin deleted')
+      
+
     chip = self.chips[mapped_pin.chip]
-    #print("chip {}, pin {}", chip.name, pin)
     p = chip.get_line(mapped_pin.pin)
-    #print(dir(p))
+    bais = gpiod.line_request.FLAG_BIAS_DISABLE
+
+    if pull_up_down == self.PUD_DOWN:
+      bais = gpiod.line_request.FLAG_BIAS_PULL_DOWN
+    elif pull_up_down == self.PUD_UP:
+      bais = gpiod.line_request.FLAG_BIAS_PULL_UP
+    else:
+      bias = gpiod.line_request.FLAG_BIAS_DISABLE
+    
     config = gpiod.line_request()
     config.consumer = "Blink"
-    config.request_type = gpiod.line_request.DIRECTION_OUTPUT if mode == self.OUT else gpiod.line_request.DIRECTION_INPUT
-    config.flags = gpiod.line_request.FLAG_BIAS_PULL_UP
+    config.request_type = gpiod.line_request.DIRECTION_OUTPUT if direction == self.OUT else gpiod.line_request.DIRECTION_INPUT
+    config.flags = bais
 
     p.request(config)
     self.used_pins[pin] = p
 
   def output(self, pin, level):
-    #print("output: ", pin, level)
-    #print(self.used_pins)
     p = self.used_pins[pin]
-    #print(dir(p))
     p.set_value(level)
     print("output set")
+
+  def input(self, channel):
+    p = self.used_pins[channel]
+    return p.get_value()
 
   def cleanup(self):
     while len(self.used_pins ) > 1:
       pinItem = self.used_pins.popitem()
-     # print("Releasing pin : ", self.used_pins)
       pinItem.release()
 
   def PWM(self, pin, frequency):
-   # self.setup(pin, self.OUT)
     p = self.used_pins[pin]
     print("Pulse", dir(Pulse))
     return Pulse.PulseWidthManagement(p, frequency)
--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-from setuptools import setup
+import setuptools
+
+with open("README.txt", 'r') as fh:
+	long_description = fh.read()
 
 classifiers = []
 
-setup(
-	name = "LePotatoPi.GPIO"
-	version = "0.0.1"
-	author = ""
-	description = "Le Potato Port of RPi.GPIO"
-	packages = ["LePotatoPi", "LePotatoPi.GPIO", "LePotatoPi.GPIO.GPIO"]
+setuptools.setup(
+	name = "LePotatoPi.GPIO",
+	version = "0.0.2",
+	author = "solrobson",
+	description = "Le Potato Port of RPi.GPIO to help allow Le Potato Developers to follow coding tutorials",
+	long_description = "Gives Libre Le Potato the same interface mapping as RPi.GPIO and the same pin numbers",
+	url = "https://github.com/solrobson/le-potato-pi",
+	packages = setuptools.find_packages(),
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"Development Status :: 2 - Pre-Alpha",
+		"Intended Audience :: Developers",
+		"Topic :: System :: Hardware"
+	],
 )
